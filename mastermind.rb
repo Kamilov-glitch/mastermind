d6082975456ed31ec6b743c923830abfd38582d3
Fuad <kamilovfuad@gmail.com>
2021-07-08 21:43:46 +0400

    Add random color generator

--- mastermind.rb
+++ mastermind.rb
@@ -14,52 +14,82 @@ module Colors
   
 	
 	@@color_palette = {
-		1 =>'black', 
-		2 => 'red', 
-		3 => 'green', 
-		4 => 'brown', 
-		5 => 'blue', 
-		6 => 'magenta', 
-		7 => 'cyan', 
-		8 => 'gray'
+		'1' =>'black', 
+		'2' => 'red', 
+		'3' => 'green', 
+		'4' => 'brown', 
+		'5' => 'blue', 
+		'6' => 'magenta', 
+		'7' => 'cyan', 
+		'8' => 'gray'
 	}
 end
 
 class Maker
-
-	def choose
-		ask_for_num
-
-	  case @num
-		when 1
-			black
-		when 2
-			red
-		when 3
-			green
-		when 4
-			brown
-		when 5
-			blue
-		when 6
-			magenta
-		when 7
-			cyan
-		when 8
-			gray
+	include Colors
+	
+	@@chosen_colors      # bu sonra yoxlama ucun lazim olasan
+	@@random_colors = []
+	def random_color_chose
+		while random_colors.length < 4
+			random_color = rand(1..6)
+			random_colors.push(random_color) unless random_colors.include?(random_color)
 		end
 	end
 
+	def self.ask_for_num
+		puts 'Please enter 4 numbers from 1 to 6 inclusive.'
+		@@num = gets.chomp.to_i
+		ask_for_num if @num.to_s.length < 4 || @num.to_s.length > 4
+	end
+	
+	# def self.make
+	# 	@@random_colors.each do |color|
+	# 		case color
+	# 		when 1
+	# 			@@chosen_colors.push(@@color_palette[1])
+	# 			black
+	# 		when 2
+	# 			@@chosen_colors.push(@@color_palette[2])
+	# 			red
+	# 		when 3
+	# 			@@chosen_colors.push(@@color_palette[3])
+	# 			green
+	# 		when 4
+	# 			@@chosen_colors.push(@@color_palette[4])
+	# 			brown
+	# 		when 5
+	# 			@@chosen_colors.push(@@color_palette[5])
+	# 			blue
+	# 		when 6
+	# 			@@chosen_colors.push(@@color_palette[6])
+	# 			magenta
+	# 		when 7
+	# 			@@chosen_colors.push(@@color_palette[7])
+	# 			cyan
+	# 		when 8
+	# 			@@chosen_colors.push(@@color_palette[8])
+	# 			gray
+	# 		end
+	# 	end
+	# end
 end
 
-class HumanBreaker
+class CompMaker < Maker
 	include Colors
+end
+
+
+
+class HumanBreaker < Maker
+	include Colors
+	@@find_attempts = []
 	def initialize(name)
 		@name = name
 	end
 
 	def ask_for_num
-		puts 'Please enter 4 numbers'
+		puts 'Please enter 4 numbers from 1 to 6 inclusive.'
 		@num = gets.chomp.to_i
 		ask_for_num if @num.to_s.length < 4 || @num.to_s.length > 4
 	end
@@ -67,23 +97,31 @@ class HumanBreaker
 	def choose
 		ask_for_num
 		@num.to_s.split("").each do |n|
-			color = @@color_palette[n.to_i]
+			color = @@color_palette[n]
 			case color
 			when 'black'
+				@@find_attempts.push(color)
 				print n.black
 			when 'red'
+				@@find_attempts.push(color)
 				print n.red
 			when 'green'
+				@@find_attempts.push(color)
 				print n.green
 			when 'brown'
+				@@find_attempts.push(color)
 				print n.brown
 			when 'blue'
+				@@find_attempts.push(color)
 				print n.blue
 			when 'magenta'
+				@@find_attempts.push(color)
 				print n.magenta
 			when 'cyan'
+				@@find_attempts.push(color)
 				print n.cyan
 			when 'gray'
+				@@find_attempts.push(color)
 				print n.gray
 			end
 		end
